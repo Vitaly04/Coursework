bbf0ab1260b46af66ea054769a6294127c16abd1
Vitaly <supercyfqgth2010@yandex.ru>
2021-08-05 04:50:48 +0300

    fixed schema.java

--- javabasicsdiploma/src/ru/netology/graphics/image/Schema.java
+++ javabasicsdiploma/src/ru/netology/graphics/image/Schema.java
@@ -4,7 +4,7 @@ public class Schema implements TextColorSchema {
     @Override
     public char convert(int color) {
         char[] chars = {'#', '$', '@', '%', '*', '+', '-'};
-        int convert = (color * chars.length - 1) / 255;
+        int convert = (color * (chars.length - 1)) / 255;
         return chars[convert];
     }
 }
